43a70437e391fd6953530341b72be279b63abbad
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-06-30 14:38:16 -0300

    Feat: Movable Ellipse and Movable interface

--- src/main/java/com/tp/poo/backend/model/Ellipse.java
+++ src/main/java/com/tp/poo/backend/model/Ellipse.java
@@ -12,6 +12,29 @@ public class Ellipse extends Figure {
     }
 
     @Override
+    public void moveX(double delta) {
+        centerPoint.moveX(delta);
+    }
+
+    @Override
+    public void moveY(double delta) {
+        centerPoint.moveY(delta);
+    }
+
+    private void setAxes(double mayor, double minor) {
+        if (mayor <= minor || mayor <= 0 || minor <= 0) {
+            throw new IllegalArgumentException("Incompatible mayor and minor axes");
+        }
+        sMayorAxis = mayor;
+        sMinorAxis = minor;
+    }
+
+    @Override
+    public void magnify(double magnificationRate) {
+        setAxes(sMayorAxis * magnificationRate, sMinorAxis * magnificationRate);
+    }
+
+    @Override
     public String toString() {
         return String.format("Elipse [Centro: %s, DMayor: %.2f, DMenor: %.2f]", centerPoint, sMayorAxis, sMinorAxis);
     }
--- src/main/java/com/tp/poo/backend/model/Movable.java
+++ src/main/java/com/tp/poo/backend/model/Movable.java
@@ -2,6 +2,11 @@ package com.tp.poo.backend.model;
 
 public interface Movable {
 
+    default void moveD(double deltaX, double deltaY) {
+        moveX(deltaX);
+        moveY(deltaY);
+    }
+
     void moveX(double delta);
 
     void moveY(double delta);
