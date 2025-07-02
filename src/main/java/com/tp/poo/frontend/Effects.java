a419f2d68eabdf95024149d6a9e2acd8a3111f11
Ivonne Choe <ivonnechoe@gmail.com>
2025-07-02 15:25:59 -0300

    .

--- src/main/java/com/tp/poo/frontend/Effects.java
+++ src/main/java/com/tp/poo/frontend/Effects.java
@@ -1,11 +1,16 @@
 package com.tp.poo.frontend;
 
+import javafx.scene.paint.Color;
 //Enum para aplicar los filtros correspondientes.
 public enum Effects {
-    SHADOW, BRIGHTENING;
+    SHADOW()
+
+    , BRIGHTENING();
+
+    private final Color color;
 
     //capaz que acepte como parametro la clase q va a contener la figura encapsulada(A crear) y aplique el effect??--> CHECK LOGICA
-    public abstract void applyEffect();
+    public Color applyEffect(Color color);
     /// La otra opcion--> public abstract Color applyEffect(); -->q recibe el mismo parametro que la otra alternativa.
 
 }
