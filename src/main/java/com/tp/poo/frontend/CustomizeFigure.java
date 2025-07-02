34ab1cc771952d3af73a94aad70851e81de25afb
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-07-02 16:30:56 -0300

    Fix: EnumSet de Efectos

--- src/main/java/com/tp/poo/frontend/CustomizeFigure.java
+++ src/main/java/com/tp/poo/frontend/CustomizeFigure.java
@@ -1,22 +1,28 @@
 package com.tp.poo.frontend;
 
+import java.util.EnumSet;
+
 import com.tp.poo.backend.model.figures.Figure;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class CustomizeFigure {
+
     private final Figure figure;
-    private Color originalColor;
-    private Color currentColor;
+    private Color color;
+    private EnumSet<Effects> filter;
     private BorderType borderType;
+
     public CustomizeFigure(Figure figure){
         this.figure = figure;
     }
-    public void getOriginalColor(GraphicsContext gc){
-        this.originalColor = (Color) gc.getFill();
+
+    public void setColor(GraphicsContext gc){
+        this.color = (Color) gc.getFill();
     }
+
     public Color getOriginalColor(){
-        return originalColor;
+        return color;
     }
 
 }
