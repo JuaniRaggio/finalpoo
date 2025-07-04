7c2e33b2483e8dc4b8bfb2627fed72a09beee483
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-07-04 18:11:49 -0300

    UIConstants y UIComponentFactory Mejorables

---
+++ src/main/java/com/tp/poo/frontend/UIComponentFactory.java
@@ -0,0 +1,126 @@
+package com.tp.poo.frontend;
+
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
+
+public final class UIComponentFactory {
+    
+    private static final int DEFAULT_BUTTON_MIN_WIDTH = 90;
+    
+    public static CheckBox createEffectCheckBox(String text) {
+        CheckBox checkBox = new CheckBox(text);
+        checkBox.setMinWidth(DEFAULT_BUTTON_MIN_WIDTH);
+        checkBox.setCursor(Cursor.HAND);
+        return checkBox;
+    }
+    
+    public static ToggleButton createToolButton(String text) {
+        ToggleButton button = new ToggleButton(text);
+        button.setMinWidth(DEFAULT_BUTTON_MIN_WIDTH);
+        button.setCursor(Cursor.HAND);
+        return button;
+    }
+    
+    public static Button createOperationButton(String text) {
+        Button button = new Button(text);
+        button.setMinWidth(DEFAULT_BUTTON_MIN_WIDTH);
+        button.setCursor(Cursor.HAND);
+        return button;
+    }
+    
+    public static Button createFormatButton(String text) {
+        Button button = new Button(text);
+        button.setMinWidth(DEFAULT_BUTTON_MIN_WIDTH);
+        button.setCursor(Cursor.HAND);
+        return button;
+    }
+    
+    public static ComboBox<BorderType> createBorderTypeComboBox() {
+        ComboBox<BorderType> comboBox = new ComboBox<>();
+        comboBox.getItems().addAll(BorderType.values());
+        comboBox.setValue(BorderType.SOLID);
+        comboBox.setMinWidth(DEFAULT_BUTTON_MIN_WIDTH);
+        comboBox.setCursor(Cursor.HAND);
+        return comboBox;
+    }
+    
+    public static ColorPicker createColorPicker() {
+        ColorPicker colorPicker = new ColorPicker(Color.YELLOW);
+        colorPicker.setMinWidth(DEFAULT_BUTTON_MIN_WIDTH);
+        colorPicker.setCursor(Cursor.HAND);
+        return colorPicker;
+    }
+    
+    public static CheckBox createShadowCheckBox() {
+        return createEffectCheckBox(UIConstants.SHADOW_BUTTON_TEXT);
+    }
+    
+    public static CheckBox createBrightenCheckBox() {
+        return createEffectCheckBox(UIConstants.BRIGHTEN_BUTTON_TEXT);
+    }
+    
+    public static CheckBox createHorizontalMirrorCheckBox() {
+        return createEffectCheckBox(UIConstants.HORIZONTAL_MIRROR_BUTTON_TEXT);
+    }
+    
+    public static CheckBox createVerticalMirrorCheckBox() {
+        return createEffectCheckBox(UIConstants.VERTICAL_MIRROR_BUTTON_TEXT);
+    }
+    
+    public static ToggleButton createSelectButton() {
+        return createToolButton(UIConstants.SELECT_BUTTON_TEXT);
+    }
+    
+    public static ToggleButton createRectangleButton() {
+        return createToolButton(UIConstants.RECTANGLE_BUTTON_TEXT);
+    }
+    
+    public static ToggleButton createCircleButton() {
+        return createToolButton(UIConstants.CIRCLE_BUTTON_TEXT);
+    }
+    
+    public static ToggleButton createSquareButton() {
+        return createToolButton(UIConstants.SQUARE_BUTTON_TEXT);
+    }
+    
+    public static ToggleButton createEllipseButton() {
+        return createToolButton(UIConstants.ELLIPSE_BUTTON_TEXT);
+    }
+    
+    public static ToggleButton createDeleteButton() {
+        return createToolButton(UIConstants.DELETE_BUTTON_TEXT);
+    }
+    
+    public static Button createDivideHButton() {
+        return createOperationButton(UIConstants.DIVIDE_H_BUTTON_TEXT);
+    }
+    
+    public static Button createDivideVButton() {
+        return createOperationButton(UIConstants.DIVIDE_V_BUTTON_TEXT);
+    }
+    
+    public static Button createMultiplyButton() {
+        return createOperationButton(UIConstants.MULTIPLY_BUTTON_TEXT);
+    }
+    
+    public static Button createTransferButton() {
+        return createOperationButton(UIConstants.TRANSFER_BUTTON_TEXT);
+    }
+    
+    public static Button createCopyFormatButton() {
+        return createFormatButton(UIConstants.COPY_FORMAT_BUTTON_TEXT);
+    }
+    
+    public static Button createPasteFormatButton() {
+        return createFormatButton(UIConstants.PASTE_FORMAT_BUTTON_TEXT);
+    }
+    
+    private UIComponentFactory() {
+        // Prevent instantiation
+    }
+} 
---
+++ src/main/java/com/tp/poo/frontend/UIConstants.java
@@ -0,0 +1,35 @@
+package com.tp.poo.frontend;
+
+public final class UIConstants {
+    public static final String SELECT_BUTTON_TEXT = "Select";
+    public static final String RECTANGLE_BUTTON_TEXT = "Rectangle";
+    public static final String CIRCLE_BUTTON_TEXT = "Circle";
+    public static final String SQUARE_BUTTON_TEXT = "Square";
+    public static final String ELLIPSE_BUTTON_TEXT = "Ellipse";
+    public static final String DELETE_BUTTON_TEXT = "Erase";
+    
+    public static final String DIVIDE_H_BUTTON_TEXT = "Divide H.";
+    public static final String DIVIDE_V_BUTTON_TEXT = "Divide V.";
+    public static final String MULTIPLY_BUTTON_TEXT = "Multiply";
+    public static final String TRANSFER_BUTTON_TEXT = "Transfer";
+    
+    public static final String COPY_FORMAT_BUTTON_TEXT = "Copy format";
+    public static final String PASTE_FORMAT_BUTTON_TEXT = "Paste format";
+    
+    public static final String SHADOW_BUTTON_TEXT = "Darken";
+    public static final String BRIGHTEN_BUTTON_TEXT = "Brighten";
+    public static final String HORIZONTAL_MIRROR_BUTTON_TEXT = "Horizontal Mirror";
+    public static final String VERTICAL_MIRROR_BUTTON_TEXT = "Vertical Mirror";
+    
+    public static final String EFFECTS_LABEL_TEXT = "Effects:";
+    public static final String OPERATIONS_LABEL_TEXT = "Operations:";
+    
+    public static final String SIDEBAR_STYLE = "-fx-background-color: #999";
+    public static final String EFFECTS_BAR_STYLE = "-fx-background-color: #999;";
+    
+    public static final String NO_FIGURE_FOUND_MESSAGE = "No figure found";
+    public static final String ERROR_PREFIX = "Error: ";
+    
+    private UIConstants() {
+    }
+} 
